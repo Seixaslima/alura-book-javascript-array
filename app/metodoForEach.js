eb08ad3bdabba078f29c4e494c05f881b0ad0c58
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-05-31 10:14:25 -0300

    exercicios unidade 2 finalizados

---
+++ app/metodoForEach.js
@@ -0,0 +1,22 @@
+const secaoDeLivros = document.getElementById('livros');
+
+function colocarLivrosNaPagina(listaDeLivros) {
+  listaDeLivros.forEach(livro => {
+    secaoDeLivros.innerHTML += `
+    <div class="livro">
+      <img class="livro__imagens ${
+        livro.quantidade > 0 ? '' : 'indisponivel'
+      }" src="${livro.imagem}"
+        alt="${livro.alt} " />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+    `;
+  });
+}
